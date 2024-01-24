a7fe0a1a35860882d4ee804790f64defb413c517
kokojong <duwhd0349@kookmin.ac.kr>
2024-01-25 00:20:53 +0900

    [init] Project 생성

---
+++ REGO/REGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  REGOApp.swift
+//  REGO
+//
+//  Created by kokojong on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct REGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
