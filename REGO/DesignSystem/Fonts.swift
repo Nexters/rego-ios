29e59027d81260ee69c081b898263f870ba05161
dam <37874238+choidam@users.noreply.github.com>
2024-01-29 01:05:27 +0900

    🔧 designSystem label 추가

--- REGO/DesignSystem/Fonts.swift
+++ REGO/DesignSystem/Fonts.swift
@@ -7,6 +7,56 @@
 
 import UIKit
 
-extension UIFont {
-    
+public typealias RegoSystemFont = RegoFontPresentation & CaseIterable
+
+public protocol RegoFontPresentation {
+    var font: UIFont { get }
+}
+
+enum RegoFontStyle: RegoSystemFont {
+    case h1, h2, h3, h4
+    case subtitle1, subtitle2, subtitle3, subtitle4, subtitle5
+    case button
+    case body1, body2, body3, body4, body5, body6, body7
+}
+
+extension RegoFontStyle {
+    var font: UIFont {
+        switch self {
+        case .h1:
+            return .systemFont(ofSize: 26, weight: .semibold)
+        case .h2:
+            return .systemFont(ofSize: 24, weight: .semibold)
+        case .h3:
+            return .systemFont(ofSize: 20, weight: .semibold)
+        case .h4:
+            return .systemFont(ofSize: 18, weight: .bold)
+        case .subtitle1:
+            return .systemFont(ofSize: 18, weight: .semibold)
+        case .subtitle2:
+            return .systemFont(ofSize: 17, weight: .semibold)
+        case .subtitle3:
+            return .systemFont(ofSize: 17, weight: .semibold)
+        case .subtitle4:
+            return .systemFont(ofSize: 16, weight: .medium)
+        case .subtitle5:
+            return .systemFont(ofSize: 15, weight: .semibold)
+        case .button:
+            return .systemFont(ofSize: 16, weight: .medium)
+        case .body1:
+            return .systemFont(ofSize: 15, weight: .regular)
+        case .body2:
+            return .systemFont(ofSize: 14, weight: .bold)
+        case .body3:
+            return .systemFont(ofSize: 14, weight: .medium)
+        case .body4:
+            return .systemFont(ofSize: 14, weight: .regular)
+        case .body5:
+            return .systemFont(ofSize: 14, weight: .regular)
+        case .body6:
+            return .systemFont(ofSize: 12, weight: .regular)
+        case .body7:
+            return .systemFont(ofSize: 10, weight: .regular)
+        }
+    }
 }
