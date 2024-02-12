201ab75b3d10750a523aa56f575dbd1de6720dcd
kokojong <duwhd0349@kookmin.ac.kr>
2024-02-13 01:28:46 +0900

    :bento: 로티 이미지 추가(하트, 로딩)

--- REGO/Scene/GameList/LikeButtonView.swift
+++ REGO/Scene/GameList/LikeButtonView.swift
@@ -13,7 +13,7 @@ struct LikeButtonView: View {
     @Binding var likeCnt: Int
 
     var body: some View {
-        VStack(spacing: -2){
+        VStack(spacing: -2) {
             Button(action: {
                 isLiked.toggle()
                 if isLiked {
@@ -23,9 +23,15 @@ struct LikeButtonView: View {
                     likeCnt -= 1
                 }
             }, label: {
-                Image(.icon24Liked)
-                    .frame(width: 24, height: 24)
-                    .foregroundStyle(isLiked ? Color(.primary500) : Color(.gray200))
+                ZStack {
+                    Image(.icon24Liked)
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(isLiked ? Color(.primary500) : Color(.gray200))
+                    if isLiked {
+                        LottieView(filename: "like_small_lottie", loopMode: .playOnce)
+                            .frame(width: 24, height: 24)
+                    }
+                }
             })
             Text(likeCnt > 999 ? "+999" : "\(likeCnt)")
                 .font(RegoFontStyle.body7.font)
