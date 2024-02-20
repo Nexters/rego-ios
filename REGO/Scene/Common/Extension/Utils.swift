ff938712b1bf239b74eff3b89d058d22f98fd43f
kokojong <duwhd0349@kookmin.ac.kr>
2024-02-20 11:54:35 +0900

    :wrench: 공통 상수 추가

---
+++ REGO/Scene/Common/Extension/Utils.swift
@@ -0,0 +1,14 @@
+//
+//  Utils.swift
+//  REGO
+//
+//  Created by kokojong on 2/19/24.
+//
+
+import UIKit
+
+final class Utils {
+    static let deviceID = UIDevice.current.identifierForVendor!.uuidString
+    static let deviceWidth = UIScreen.main.bounds.size.width
+    static let deviceHeight = UIScreen.main.bounds.size.height
+}
